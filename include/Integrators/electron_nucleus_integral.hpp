068f7e5240952f0efa6c1b9ab1b94d45bf395ddc
alexandremnd <alexxandre.mnd+dev@gmail.com>
2024-12-02 21:10:41 +0100

    Changed electron-nucleus integral definition

--- include/Integrators/electron_nucleus_integral.hpp
+++ include/Integrators/electron_nucleus_integral.hpp
@@ -3,17 +3,17 @@
 #include "include/BasisSet/gaussian_primitive.hpp"
 #include "include/BasisSet/slater_primitive.hpp"
 
+#include "include/Eigen/Dense"
+
 template <typename T>
-double electron_nucleus_integral(const T& orbital1, const T& orbital2, const T& orbital3, const T& orbital4);
+double electron_nucleus_integral(const T& orbital1, const T& orbital2, const Eigen::Vector3d& nucleus_position);
 
 template <>
 double electron_nucleus_integral(const GaussianPrimitive& orbital1,
                                 const GaussianPrimitive& orbital2,
-                                const GaussianPrimitive& orbital3,
-                                const GaussianPrimitive& orbital4);
+                                const Eigen::Vector3d& nucleus_position);
 
 template <>
 double electron_nucleus_integral(const SlaterPrimitive& orbital1,
                                 const SlaterPrimitive& orbital2,
-                                const SlaterPrimitive& orbital3,
-                                const SlaterPrimitive& orbital4);
+                                const Eigen::Vector3d& nucleus_position);
