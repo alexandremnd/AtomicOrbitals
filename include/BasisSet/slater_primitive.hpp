f333e66ee7543d42a1439ad65adb379714749e25
alexandremnd <alexxandre.mnd+dev@gmail.com>
2024-12-03 22:40:30 +0100

    Add Slater orbital definition

--- include/BasisSet/slater_primitive.hpp
+++ include/BasisSet/slater_primitive.hpp
@@ -1,5 +1,22 @@
 #pragma once
 
+#include <boost/math/special_functions/gamma.hpp>
+
 class SlaterPrimitive {
+public:
+    SlaterPrimitive(const int n, const int l, const int m, const double alpha) : m_alpha(alpha), m_n(n), m_l(l), m_m(m) {
+        normalization_constant = std::pow(2 * alpha, n + 0.5) / std::sqrt(boost::math::tgamma(2 * n + 1));
+    }
+
+    int n() const { return m_n; }
+    int l() const { return m_l; }
+    int m() const { return m_m; }
+    double alpha() const { return m_alpha; }
+    double normalization() const { return normalization_constant; }
+
+private:
+    int m_n, m_l, m_m;
+    double m_alpha;
+    double normalization_constant;
 
 };
