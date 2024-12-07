29b12676df5759992b677665e2b66cb1ef329c7e
alexandremnd <alexxandre.mnd+dev@gmail.com>
2024-12-07 17:55:02 +0100

    Delete factorial and add overlap integral for Slater Primitive
    Factorials are replaced by boost::math::tgamma for consistency.

--- src/Integrators/overlap_integral.cpp
+++ src/Integrators/overlap_integral.cpp
@@ -6,5 +6,9 @@ double overlap_integral(const GaussianPrimitive& orbital1, const GaussianPrimiti
 }
 
 double overlap_integral(const SlaterPrimitive& orbital1, const SlaterPrimitive& orbital2) {
-    throw std::logic_error("Not implemented");
+    if (orbital1.l() != orbital2.l() || orbital1.m() != orbital2.m()) {
+        return 0.;
+    }
+
+    return orbital1.normalization() * orbital2.normalization() * boost::math::tgamma(orbital1.n() + orbital2.n() + 1) * std::pow(orbital1.alpha() + orbital2.alpha(), -orbital1.n() - orbital2.n() - 1);
 }
