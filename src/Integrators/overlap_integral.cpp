4289e1f7ce24d1db25106efa518f8cad32a1daa0
alexandremnd <alexxandre.mnd+dev@gmail.com>
2024-12-14 19:50:29 +0100

    Add overlap_integral test
    - Update CMakeList to reuse already compiled object files
    - Update overlap_integral definition

--- src/Integrators/overlap_integral.cpp
+++ src/Integrators/overlap_integral.cpp
@@ -5,10 +5,14 @@ double overlap_integral(const GaussianPrimitive& orbital1, const GaussianPrimiti
     throw std::logic_error("Not implemented");
 }
 
+double overlap_integral(const ContractedGaussian& orbital1, const ContractedGaussian& orbital2) {
+    throw std::logic_error("Not implemented");
+}
+
 double overlap_integral(const SlaterPrimitive& orbital1, const SlaterPrimitive& orbital2) {
     if (orbital1.l() != orbital2.l() || orbital1.m() != orbital2.m()) {
         return 0.;
     }
 
-    return orbital1.normalization() * orbital2.normalization() * boost::math::tgamma(orbital1.n() + orbital2.n() + 1) * std::pow(orbital1.alpha() + orbital2.alpha(), -orbital1.n() - orbital2.n() - 1);
+    return boost::math::tgamma(orbital1.n() + orbital2.n() + 1) * std::pow(orbital1.alpha() + orbital2.alpha(), -orbital1.n() - orbital2.n() - 1);
 }
---
+++ test/overlap_test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include "BasisSet/slater_primitive.hpp"
+#include "Integrators/overlap_integral.hpp"
+
+TEST(OverlapIntegralTest, SlaterPrimitiveZeroCase) {
+    SlaterPrimitive orbital1(1, 0, 0, 0.5);
+    SlaterPrimitive orbital2(5, 1, 0, 1.3);
+
+    EXPECT_NEAR(overlap_integral(orbital1, orbital2), 0., 1e-2);
+
+    orbital1.set_l(1); orbital1.set_m(0);
+    orbital2.set_l(1); orbital2.set_m(1);
+
+    EXPECT_NEAR(overlap_integral(orbital1, orbital2), 0., 1e-2);
+}
+
+TEST(OverlapIntegralTest, SlaterPrimitiveValue) {
+    SlaterPrimitive orbital1(1, 0, 0, 0.5);
+    SlaterPrimitive orbital2(5, 0, 0, 1.3);
+
+    EXPECT_NEAR(overlap_integral(orbital1, orbital2), 11.76047764474325, 1e-5);
+
+    orbital1.set_n(3); orbital2.set_n(8);
+    EXPECT_NEAR(overlap_integral(orbital1, orbital2), 34505.28801422153, 1e-3);
+}
