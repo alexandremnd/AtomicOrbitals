74fc59a929f19a2dbfe9b846675d3d5638aa767c
alexandremnd <alexxandre.mnd+dev@gmail.com>
2024-12-27 12:33:19 +0100

    More documentation for slater_braket

--- include/Integrators/slater_braket.hpp
+++ include/Integrators/slater_braket.hpp
@@ -19,19 +19,19 @@
     \f]
  * where \f$ r_< = \min(r_1, r_2) \f$ and \f$ r_> = \max(r_1, r_2) \f$
  *
- * @param orbital1 First Slater primitive
- * @param orbital2 Second Slater primitive
- * @param orbital3 Third Slater primitive
- * @param orbital4 Fourth Slater primitive
+ * @param orbital_i First Slater primitive
+ * @param orbital_j Second Slater primitive
+ * @param orbital_k Third Slater primitive
+ * @param orbital_l Fourth Slater primitive
  * @param L Angular momentum quantum number
  * @return double Evaluation of the radial integral
  */
-inline double radial_integral(const SlaterPrimitive &orbital1, const SlaterPrimitive &orbital2, const SlaterPrimitive &orbital3, const SlaterPrimitive &orbital4, const int L) {
-    double n_ik = orbital1.n() + orbital3.n();
-    double n_jl = orbital2.n() + orbital4.n();
+inline double radial_integral(const SlaterPrimitive &orbital_i, const SlaterPrimitive &orbital_j, const SlaterPrimitive &orbital_k, const SlaterPrimitive &orbital_l, const int L) {
+    double n_ik = orbital_i.n() + orbital_k.n();
+    double n_jl = orbital_j.n() + orbital_l.n();
 
-    double alpha_ik = orbital1.alpha() + orbital3.alpha();
-    double alpha_jl = orbital2.alpha() + orbital4.alpha();
+    double alpha_ik = orbital_i.alpha() + orbital_k.alpha();
+    double alpha_jl = orbital_j.alpha() + orbital_l.alpha();
 
     double prefactor = boost::math::tgamma(n_ik + n_jl + 1) * std::pow(alpha_ik + alpha_jl, -n_ik - n_jl - 1);
 
@@ -47,7 +47,7 @@ inline double radial_integral(const SlaterPrimitive &orbital1, const SlaterPrimi
  * Integral is defined as:
  *  \f[
  *       I_2(L, M) = \int d\Omega
- *       Y_{l_1}^{m_1} (\theta_1, \phi_1)^* Y_{l_2}^{m_2} (\theta_1, \phi_1) Y_{L}^{M} (\theta_1, \phi_1)
+ *       Y_{l_i}^{m_i} (\theta_1, \phi_1)^* Y_{l_k}^{m_k} (\theta_1, \phi_1) Y_{L}^{M} (\theta_1, \phi_1)
  *  \f]
  * where \f$ Y_{l_1}^{m_1} (\theta_1, \phi_1) \f$ and \f$ Y_{l_2}^{m_2} (\theta_1, \phi_1) \f$ are the spherical harmonics of the first and second Slater primitives, respectively
  *
@@ -57,10 +57,10 @@ inline double radial_integral(const SlaterPrimitive &orbital1, const SlaterPrimi
  * @param M Magnetic quantum number
  * @return double Evaluation of the angular integral
  */
-inline double angular_integral(const SlaterPrimitive &orbital1, const SlaterPrimitive &orbital2, const int L, const int M) {
-    double phase_factor = (orbital1.m() % 2 == 0) ? 1. : -1.;
+inline double angular_integral(const SlaterPrimitive &orbital_i, const SlaterPrimitive &orbital_k, const int L, const int M) {
+    double phase_factor = (orbital_i.m() % 2 == 0) ? 1. : -1.;
 
-    return phase_factor * std::sqrt( (2 * orbital1.l() + 1) * (2 * orbital2.l() + 1) * (2 * L + 1) / (4 * M_PI) )
-    * Math::wigner_3j(orbital1.l(), orbital2.l(), L, 0, 0, 0)
-    * Math::wigner_3j(orbital1.l(), orbital2.l(), L, -orbital1.m(), orbital2.m(), M);
+    return phase_factor * std::sqrt( (2 * orbital_i.l() + 1) * (2 * orbital_k.l() + 1) * (2 * L + 1) / (4 * M_PI) )
+    * Math::wigner_3j(orbital_i.l(), orbital_k.l(), L, 0, 0, 0)
+    * Math::wigner_3j(orbital_i.l(), orbital_k.l(), L, -orbital_i.m(), orbital_k.m(), M);
 }
