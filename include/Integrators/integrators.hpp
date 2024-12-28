08a0229254fd9f5d520aa6291371f92dcb953110
alexandremnd <alexxandre.mnd+dev@gmail.com>
2024-12-28 14:56:05 +0100

    Delete integrators.hpp
    Unused file and does not enforce "include what you use"

--- include/Integrators/integrators.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Integrators/electron_electron_integral.hpp"
-#include "Integrators/electron_nucleus_integral.hpp"
-#include "Integrators/laplacian_integral.hpp"
-#include "Integrators/exchange_integral.hpp"
-#include "Integrators/overlap_integral.hpp"
