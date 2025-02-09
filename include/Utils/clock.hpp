42ec2224d648d6223821eb50c475a311d826cd99
alexandremnd <alexxandre.mnd+dev@gmail.com>
2025-02-09 14:48:15 +0100

    Update coding style in Clock.hpp

--- include/Utils/clock.hpp
+++ include/Utils/clock.hpp
@@ -4,18 +4,18 @@
 #include <iostream>
 
 class Clock {
-public:
+  public:
     Clock() : m_start(std::chrono::high_resolution_clock::now()) {}
 
-    void reset() {
-        m_start = std::chrono::high_resolution_clock::now();
-    }
+    void reset() { m_start = std::chrono::high_resolution_clock::now(); }
 
-    void time(const std::string& message) {
+    void time(const std::string &message) {
         auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - m_start);
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+            end - m_start);
         std::cout << message << " " << duration.count() << "us" << std::endl;
     }
-private:
+
+  private:
     std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
 };
