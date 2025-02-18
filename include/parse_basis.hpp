03862e63d60b4e1d06d3c6df2f0c1c7c78aae8f5
Alexandre Menard <alexxandre.mnd+dev@gmail.com>
2025-02-18 22:42:05 +0100

    Add documentation to parse_basis

--- include/parse_basis.hpp
+++ include/parse_basis.hpp
@@ -6,6 +6,12 @@
 
 /**
  * @brief Loads a basis file and parses the basis set into the passed atom.
+ * @note Basis file is expected to be in the data/BasisSet/basis_name directory
+ * from the current working directory. Working directory is the directory from
+ * which the executable is run.
+ *
+ * @note Basis file is expected to be named as the lowercase of the element name
+ * followed by the .basis extension.
  *
  * @param elt Element to parse basis for
  * @param basis_name Name of the basis set to load
@@ -16,6 +22,12 @@ void parse_basis(Element elt, std::string basis_name,
 
 /**
  * @brief Loads a basis file and parses the basis set into the passed atom.
+ * @note Basis file is expected to be in the data/BasisSet/basis_name directory
+ * from the current working directory. Working directory is the directory from
+ * which the executable is run.
+ *
+ * @note Basis file is expected to be named as the lowercase of the element name
+ * followed by the .basis extension.
  *
  * @param elt Element to parse basis for
  * @param basis_name Name of the basis set to load
