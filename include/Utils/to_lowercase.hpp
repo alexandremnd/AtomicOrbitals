7e89f4b1d334e7bb34496c3468778782bae6fbb5
Alexandre Menard <alexxandre.mnd+dev@gmail.com>
2025-02-18 21:28:19 +0100

    Add documentation to lowercase function and remove Primitive type in Concepts

--- include/Utils/to_lowercase.hpp
+++ include/Utils/to_lowercase.hpp
@@ -2,4 +2,10 @@
 
 #include <string>
 
+/**
+ * @brief Converts a string to lowercase
+ *
+ * @param str String to be lowercased
+ * @return std::string Lowercased string
+ */
 std::string to_lowercase(const std::string &str);
--- include/concepts.hpp
+++ include/concepts.hpp
@@ -5,14 +5,12 @@
 
 // Declare the template class for all orbital type
 #define DECLARE_TEMPLATE(TemplateClass)                                        \
-    template class TemplateClass<SlaterPrimitive>;                             \
     template class TemplateClass<ContractedSlater>;                            \
-    // template class TemplateClass<ContractedGaussian>;
+    template class TemplateClass<ContractedGaussian>;
 
 #define DECLARE_EXTERN_TEMPLATE(TemplateClass)                                 \
-    extern template class TemplateClass<SlaterPrimitive>;                      \
     extern template class TemplateClass<ContractedSlater>;                     \
-    // extern template class TemplateClass<ContractedGaussian>;
+    extern template class TemplateClass<ContractedGaussian>;
 
 template <typename T>
 concept DerivedFromOrbital = std::derived_from<T, Orbital>;
