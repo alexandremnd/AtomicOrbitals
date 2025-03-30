166a358956643a3fa5ed1f024830aaf74b37e3a5
alexandremnd <alexxandre.mnd+dev@gmail.com>
2025-03-30 16:21:52 +0200

    Adds documentation to sol_radiale

--- include/Numerov/sol_radiale.hpp
+++ include/Numerov/sol_radiale.hpp
@@ -1,3 +1,16 @@
 #pragma once
 
+/**
+ * @brief Computes the radial solution for an atomic orbital
+ *
+ * Calculates the radial wavefunction for an atomic orbital using the Numerov
+ * method to solve the radial Schrödinger equation for a hydrogen-like atom.
+ *
+ * @param Z The atomic number of the element
+ * @param l The angular momentum quantum number
+ * @param n The principal quantum number
+ *
+ * @note This function may store the results internally or output them to a file
+ * @see Numerov algorithm for differential equations
+ */
 void radial_solution(int Z, int l, int n);
